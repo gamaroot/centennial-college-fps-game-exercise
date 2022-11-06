3fc063468c68210d157568f2326e8a3e5ef7eeb5
jim80 <van0711@gmail.com>
2022-11-06 11:10:08 -0500

    Now spawns face to the player.

--- Assets/Project/Scripts/Game/Spawner.cs
+++ Assets/Project/Scripts/Game/Spawner.cs
@@ -4,6 +4,7 @@ namespace game
 {
     public class Spawner : MonoBehaviour
     {
+        [SerializeField] private Transform m_player;
         [SerializeField] private Spawnable[] m_spawnables;
 
         private void OnTriggerEnter(Collider other)
@@ -12,6 +13,7 @@ namespace game
             Transform spawnTransform = Instantiate(spawn).transform;
             spawnTransform.SetParent(base.transform.parent);
             spawnTransform.position = base.transform.position;
+            spawnTransform.LookAt(this.m_player.position);
 
             Destroy(base.gameObject);
         }
